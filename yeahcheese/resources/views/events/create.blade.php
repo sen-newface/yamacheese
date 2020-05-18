b485abb3b87103a55a387df028bfbe30432792b5
yamada <chika19103@gmail.com>
2020-05-18 20:10:43 +0900

    バリデーションエラーでリダイレクトした時に入力値を保持するように

--- yeahcheese/resources/views/events/create.blade.php
+++ yeahcheese/resources/views/events/create.blade.php
@@ -6,15 +6,15 @@
         {{ csrf_field() }}
         <div>
             <label>イベント名</label><br>
-            <input type="text" name="name" />
+            <input type="text" name="name" value="{{old('name')}}" />
         </div>
         <div>
             <label>公開開始日</label><br>
-            <input type="date" name="start_at" />
+            <input type="date" name="start_at" value="{{old('start_at')}}" />
         </div>
         <div>
             <label>公開終了日</label><br>
-            <input type="date" name="end_at" />
+            <input type="date" name="end_at" value="{{old('end_at')}}" />
         </div>
         <div>
             <input type="submit" value="作成する" />
