92213371c430b0c1e3b4dc6647098faa65d0dc12
yamada <chika19103@gmail.com>
2020-05-18 09:55:16 +0900

    共通テンプレートを使うように

--- yeahcheese/resources/views/events/index.blade.php
+++ yeahcheese/resources/views/events/index.blade.php
@@ -1,21 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset='utf-8'>
-        <title>event list</title>
-        <style>body {padding: 10px;}</style>
-    </head>
-    <body>
-        <h1>イベント一覧</h1>
-        @foreach ($events as $event)
-            <p>
-                {{ $event->id }},
-                {{ $event->name }},
-                {{ $event->start_at }},
-                {{ $event->end_at }},
-                {{ $event->authorization_key }},
-                {{ $event->user_id }}
-            </p>
-        @endforeach
-    </body>
-</html>
+@extends('events.layout')
+
+@section('content')
+    <h1>イベント一覧</h1>
+    @foreach ($events as $event)
+        <p>
+            {{ $event->id }},
+            {{ $event->name }},
+            {{ $event->start_at }},
+            {{ $event->end_at }},
+            {{ $event->authorization_key }},
+            {{ $event->user_id }}
+        </p>
+    @endforeach
+@endsection
