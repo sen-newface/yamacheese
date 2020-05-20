d72abf0ccf686c2c5226e6b0a27d3f7e20899e55
yamada <chika19103@gmail.com>
2020-05-20 13:05:21 +0900

    イベントIDカラムを追加・外部キー制約をつける

--- yeahcheese/database/migrations/2020_05_20_035340_create_photos_table.php
+++ yeahcheese/database/migrations/2020_05_20_035340_create_photos_table.php
@@ -16,7 +16,10 @@ class CreatePhotosTable extends Migration
         Schema::create('photos', function (Blueprint $table) {
             $table->id();
             $table->string('path')->comment('画像置き場のpath');
+            $table->unsignedBigInteger('event_id')->comment('イベントID');
             $table->timestamps();
+
+            $table->foreign('event_id')->references('id')->on('events');
         });
     }
 
