051ef16e085a4e7600aface5e8af920147d47dc4
yamada <chika19103@gmail.com>
2020-05-20 13:19:11 +0900

    EventとPhotoに一対多関係があるのでそれぞれにリレーション関数を定義

--- yeahcheese/app/Photo.php
+++ yeahcheese/app/Photo.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Photo extends Model
 {
-    //
+    /**
+     * この写真が掲載されているイベントを取得
+     *
+     * @return BelongsTo
+     */
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo('App\Event');
+    }
 }
