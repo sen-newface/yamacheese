c2fcae60338ceb2acdd582fe310124d84cc4bba7
yamada <chika19103@gmail.com>
2020-05-21 12:15:20 +0900

    元々登録されているデータをフォームに表示しておくように

--- yeahcheese/resources/views/events/edit.blade.php
+++ yeahcheese/resources/views/events/edit.blade.php
@@ -19,15 +19,15 @@
         <input type='hidden' name='user_id' value='{{ $event->user_id }}' />
         <div>
             <label>イベント名</label><br>
-            <input type="text" name="name" value="{{old('name')}}" />
+            <input type="text" name="name" value="{{old('name', $event->name)}}" />
         </div>
         <div>
             <label>公開開始日</label><br>
-            <input type="date" name="start_at" value="{{old('start_at')}}" />
+            <input type="date" name="start_at" value="{{old('start_at', $event->start_at)}}" />
         </div>
         <div>
             <label>公開終了日</label><br>
-            <input type="date" name="end_at" value="{{old('end_at')}}" />
+            <input type="date" name="end_at" value="{{old('end_at', $event->end_at)}}" />
         </div>
         <div>
             <input type="submit" value="作成する" />
