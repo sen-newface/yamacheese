a7fcb293005e284583fb7598d6070c8a77125c8c
yamada <chika19103@gmail.com>
2020-05-21 11:59:00 +0900

    編集・更新のルーティングを追加

--- yeahcheese/routes/web.php
+++ yeahcheese/routes/web.php
@@ -25,4 +25,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/events', 'EventController@index')->name('events.index');
     Route::get('/events/create', 'EventController@create')->name('events.create');
     Route::post('/events', 'EventController@store')->name('events.store');
+
+    Route::get('/events/{event}/edit', 'EventController@edit')->name('events.edit');
+    Route::put('/events/{event}/edit', 'EventController@update')->name('events.update');
 });
