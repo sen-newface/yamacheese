9433f281452a53cca245d88b19764a6928cdb5d6
yamada <chika19103@gmail.com>
2020-05-21 12:22:15 +0900

    update追加（バリデーションはstoreと同じ）

--- yeahcheese/app/Http/Controllers/EventController.php
+++ yeahcheese/app/Http/Controllers/EventController.php
@@ -78,12 +78,19 @@ class EventController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'filled',
+            'start_at' => 'filled|required_with:end_at|date_format:Y-m-d',
+            'end_at' => 'filled|required_with:start_at|after_or_equal:start_at|date_format:Y-m-d',
+        ]);
+
+        $event = Event::find($request->id);
+        $event->fill($request->all())->save();
+        return redirect()->route('events.edit', $event);
     }
 
     /**
