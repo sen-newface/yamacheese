68d087c271480a64e4ca505335b17cd82ed4c7b5
yamada <chika19103@gmail.com>
2020-05-25 22:00:09 +0900

    storeメソッド追加

--- yeahcheese/app/Http/Controllers/Api/PhotoController.php
+++ yeahcheese/app/Http/Controllers/Api/PhotoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Photo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PhotoController extends Controller
 {
@@ -25,9 +26,23 @@ class PhotoController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $id)
     {
-        //
+        
+        $this->validate($request, [
+            'file' => 'required|image'
+        ], [
+            'file.required' => '画像が選択されていません',
+            'file.image' => '画像ファイルではありません',
+        ]);
+
+        if ($request->file) {
+            $photo = new Photo();
+            $photo->path = Storage::putFile('public', $request->file);  // 画像を保存
+            $photo->event_id = $id;
+            $photo->save();  // DBに追加
+            return ['success' => '登録しました!'];
+        }
     }
 
     /**
