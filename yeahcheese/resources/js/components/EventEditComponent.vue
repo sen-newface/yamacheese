41dbd1b17ea825cd5634fda8c4f9bb5faef22812
yamada <chika19103@gmail.com>
2020-05-26 15:09:27 +0900

    とりあえずイベント編集画面をvue.jsにした（eventedit-componentを作成）

---
+++ yeahcheese/resources/js/components/EventEditComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div>
+                    <label>イベント名</label><br>
+                    <input type="text" name="name" />
+                </div>
+                <div>
+                    <label>公開開始日</label><br>
+                    <input type="date" name="start_at"  />
+                </div>
+                <div>
+                    <label>公開終了日</label><br>
+                    <input type="date" name="end_at"  />
+                </div>
+                <div>
+                    <input type="submit" value="更新する" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
