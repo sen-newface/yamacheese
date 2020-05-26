de1bf3ba4b290511471e03a5d101c6a0800240b6
yamada <chika19103@gmail.com>
2020-05-26 11:06:24 +0900

    update処理をAPI側に移し、返り値はリダイレクトではなく更新したイベントの値を返すように。

--- yeahcheese/app/Http/Controllers/Api/EventController.php
+++ yeahcheese/app/Http/Controllers/Api/EventController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Event;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -46,9 +47,17 @@ class EventController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'filled',
+            'start_at' => 'filled|required_with:end_at|date_format:Y-m-d',
+            'end_at' => 'filled|required_with:start_at|after_or_equal:start_at|date_format:Y-m-d',
+        ]);
+
+        $event = Event::find($request->id);
+        $event->fill($request->all())->save();
+        return $event;
     }
 
     /**
