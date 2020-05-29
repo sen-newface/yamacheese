d5de3f5b87c00eb74863ea3854dc3492b12639d1
yamada <chika19103@gmail.com>
2020-05-29 15:14:59 +0900

    updateメソッドのテスト追加

--- yeahcheese/tests/Feature/Api/EventControllerTest.php
+++ yeahcheese/tests/Feature/Api/EventControllerTest.php
@@ -32,4 +32,38 @@ class EventControllerTest extends TestCase
             'user_id' => 1
         ]);
     }
+
+    /**
+     * update
+     */
+
+    /**
+     * @testdox イベントのデータが更新出来る。
+     */
+    public function testSuccessUpdate()
+    {
+        $event = factory(Event::class)->create();
+
+        $response = $this->putJson('/api/events/' . $event->id, [
+            'name' => 'ceremony',
+            'start_at' => '2019-05-05',
+            'end_at' => '2020-05-05',
+        ]);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('events', [
+            'name' => 'ceremony',
+            'start_at' => '2019-05-05',
+            'end_at' => '2020-05-05',
+            'authorization_key' => '11111111',
+            'user_id' => 1
+        ]);
+        $response->assertJson([
+            'name' => 'ceremony',
+            'start_at' => '2019-05-05',
+            'end_at' => '2020-05-05',
+            'authorization_key' => '11111111',
+            'user_id' => 1
+        ]);
+    }
 }
