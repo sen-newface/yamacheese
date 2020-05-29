1ee6e0de3e3ab9f0a3278cca814e7d3e73643e60
yamada <chika19103@gmail.com>
2020-05-29 15:09:21 +0900

    showメソッドのテスト追加

--- yeahcheese/tests/Feature/Api/EventControllerTest.php
+++ yeahcheese/tests/Feature/Api/EventControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Api;
 
+use App\Event;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -11,14 +12,24 @@ class EventControllerTest extends TestCase
     use RefreshDatabase;
 
     /**
-     * A basic feature test example.
-     *
-     * @return void
+     * show
      */
-    public function testExample()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    /**
+     * @testdox 指定したidのイベントのデータが取得出来る。
+     */
+    public function testSuccessShow()
+    {
+        $event = factory(Event::class)->create();
+
+        $ret = $this->get('/api/events/' . $event->id);
+        $ret->assertSuccessful();
+        $ret->assertJson([
+            'name' => 'party',
+            'start_at' => '2019-10-10',
+            'end_at' => '2020-04-01',
+            'authorization_key' => '11111111',
+            'user_id' => 1
+        ]);
     }
 }
